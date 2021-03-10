47940ed92ab6b1b58c28b264fc6367a1382b3fc5
Torres <torrespjgt@gmail.com>
2021-03-10 12:03:38 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function twoNumberSum(array, targetSum) {
+	let sum = 0
+	for(let data of array){
+		sum += data
+	}
+	
+  // Write your code here.
+}
+
+// Do not edit the line below.
+exports.twoNumberSum = twoNumberSum;
